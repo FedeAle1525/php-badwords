d0fa345031ccd9a8d4a1bed5d9cedf97d39b8f01
FedeAle1525 <fedeale.1525@gmail.com>
2023-03-17 16:21:41 +0100

    PHP print 'censored text' in file "censura.php"

--- censura.php
+++ censura.php
@@ -1,1 +1,31 @@
-Paragrafo da censurare
+<?php
+
+// Stampo l'Array Associativo con all'interno i paramentri ricevuti dalla chiamata al Server (Query Selector)
+var_dump($_GET);
+
+// Creo Variabile con all'interno il Paramentro "paragraph" dentro l'Array Associativo
+$text = $_GET["paragraph"];
+// Salvo in una Variabile la lunghezza del paragrafo utilizzando una la Funzione "strlen()"
+$text_len = strlen($text);
+// Salvo in una Variabile il Paramentro "word" dentro Array Associativo
+$word_ban = $_GET["word"];
+// Salvo in una Variabile il Testo Censurato grazie alla Funzione "str_replace()"
+$text_ban = str_replace($word_ban, "***", $text);
+
+?>
+
+<div>
+
+  <!-- Utilizzo TAG <php> per usare il codice dentro HTML -->
+  <!-- Utilizzo la Funzione di Stampa "echo" per stampare la Variabile $text   -->
+  <!-- Stampo la lunghezza del paragrafo utilizzando il linguaggio php -->
+  <?php echo $text ?> ---- (Lunghezza = <?php echo $text_len ?>)
+
+</div>
+
+<div>
+
+  <!-- Stampo il Testo censurato utilizzando il linguaggio php -->
+  <?php echo $text_ban ?>
+
+</div>
--- index.php
+++ index.php
@@ -1,7 +1,9 @@
 <form action="censura.php" method="get">
 
+  <!-- Importante attributo "name" perche' sara' il nome utilizzato per i paramentri del metodo "get" -->
   <textarea name="paragraph" rows="5" cols="50" placeholder="Inserisci del testo"></textarea><br>
 
+  <!-- Importante attributo "name" perche' sara' il nome utilizzato per i paramentri del metodo "get" -->
   <input name="word" type="textarea" placeholder="Parola da censurare">
 
   <input type="submit">
