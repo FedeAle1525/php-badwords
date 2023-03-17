5563b1ecc52865d39547c35fd7efa0b7263ae3ef
FedeAle1525 <fedeale.1525@gmail.com>
2023-03-17 15:43:14 +0100

    PHP create file "index" with form

---
+++ censura.php
@@ -0,0 +1,1 @@
+Paragrafo da censurare
---
+++ index.php
@@ -0,0 +1,9 @@
+<form action="censura.php" method="get">
+
+  <textarea name="paragraph" rows="5" cols="50" placeholder="Inserisci del testo"></textarea><br>
+
+  <input name="word" type="textarea" placeholder="Parola da censurare">
+
+  <input type="submit">
+
+</form>
