f1d1c0e4811ccfd4e697655e3203d04cb386fcc2
FedeAle1525 <fedeale.1525@gmail.com>
2023-03-17 16:26:47 +0100

    REFACTOR

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
   <textarea name="paragraph" rows="5" cols="50" placeholder="Inserisci del testo"></textarea><br>
 
   <!-- Importante attributo "name" perche' sara' il nome utilizzato per i paramentri del metodo "get" -->
-  <input name="word" type="textarea" placeholder="Parola da censurare">
+  <input name="word" type="text" placeholder="Parola da censurare">
 
   <input type="submit">
 
